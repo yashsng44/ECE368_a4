92460bbeaee08dde9a51080bc1eec5e059004106
Yashashwini Singh <you@example.com>
2024-09-20 18:20:12 -0400

    extra node with 0?

--- a4.c
+++ a4.c
@@ -5,74 +5,90 @@
 #include <regex.h>
 #include "a4.h"
 // //regex_t regex;
+Node * search_for_node (Header * header, int target) {
+    Node * search_node = header->head;
 
+    while(search_node != NULL && search_node->data != target) {
+        search_node = search_node->next;
+    }
+
+    return search_node;
+}
 void open_window (Header * header, Node * newNode) {
     Node * dummy_node = malloc(sizeof(Node));
+    int target = newNode->data;
+    Node * searched = search_for_node(header, target);
+    if (searched == NULL) {
     dummy_node = header->head;
     header->head = newNode;
     newNode->next = dummy_node;
+    } 
     return;
 }
 
 void switch_window (Header * header, int target) {
     Node * search_node = header->head;
-    Node * dummy = NULL;
     Node * prev = NULL;
 
     while(search_node != NULL && search_node->data != target) {
         prev = search_node;
         search_node = search_node->next;
     }
-    // found the node!
-    // not deleting/rerouting properly...
+
     if (search_node) {
-        if (prev == NULL) {
-        return; // make no switches if the tab you want to switch to is the same one..
-        header->head = search_node->next;
-        } else {
+        if (prev !=NULL) {
         prev->next = search_node->next;
-        dummy = search_node;
-        free(search_node);
-        }
+        open_window(header, search_node);
+        } 
     }
-    open_window(header, dummy);
     return;
 }
 
 void close_window (Header * header, int target) {
-    Node * search_node = header->head;
-    Node * dummy = header -> head;
+    Node * search_node = header->head; // creates a copy of the header head..
     Node * prev = NULL;
+
+    if (search_node == NULL) {
+        header->head = NULL;
+        return;
+    }
 
     while(search_node != NULL && search_node->data != target) {
         prev = search_node;
         search_node = search_node->next;
     }
-    header->head = search_node;
-    
 
+    if (search_node) {
+        if (prev == NULL && search_node == header->head) {
+        header->head = header->head->next; // shift the header to the next node; closing the top node.
+    } else {
+        prev->next = search_node->next;
+        }
+    }
+    return;
 }
+
 void output_window (char * window_spec, Header * header) {
     int number;
 
     if (sscanf(window_spec, "%*[^0-9]%d", &number)) {
-
-    if(strcmp(window_spec, "open")) {
-        // the only case where its necessary to create a new node...
+        
+    if(strstr(window_spec, "open") == window_spec) {
         Node * newNode = malloc(sizeof(Node));
         newNode->data = number;
         newNode->next = NULL;
         open_window(header, newNode);
-        printf("%d\n", header->head->data);
-    } else if (strcmp(window_spec, "switch"))   {
+    } else if (strstr(window_spec, "switch") == window_spec)   {
         switch_window(header, number);
-        printf("%d\n", header->head->data);
-    } else if (strcmp(window_spec, "close")) {
+    } else if (strstr(window_spec, "close") == window_spec) {
+        close_window(header, number);
+    } else {
     }
     } else {
         printf("No valid number input \n");
         return;
     }
 
+    printf("%d\n", header->head->data);
     return;
 }
--- main.c
+++ main.c
@@ -6,31 +6,26 @@
 
 int main(void) {
 
-    int t = 0;
+
     char window_spec[1000];
     struct Node * head = malloc(sizeof(Node));
     struct Header * header = malloc(sizeof(Header));
     header->head = head;
     
     while(header->head != NULL) {
-
         if(fgets(window_spec, sizeof(window_spec), stdin)){
             window_spec[strcspn(window_spec, "\n")] = 0;
             output_window(window_spec, header);
         } else {
             return EXIT_FAILURE;
         }
-        if (t == 3) {
-            break;
-        }
-        t++;
     }
 
-
-    while(header->head != NULL) {
+     while(header->head != NULL) {
         printf("%d\n", header->head->data);
         header->head = header->head->next;
     }
+
 
     return EXIT_SUCCESS;
 }
